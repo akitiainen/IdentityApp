b7ab2f530741aaf12aa81ba03c5fc94ee55ea7c7
akitiainen <42929899+akitiainen@users.noreply.github.com>
2022-02-24 12:36:33 +0200

    Initial commit

---
+++ IdentityApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IdentityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringHandler stringHandler = new StringHandler();
+            Console.WriteLine("Hello World!");
+            stringHandler.ParseURI();
+        }
+    }
+}
---
+++ IdentityApp/StringHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityApp
+{
+    class StringHandler
+    {
+        private string _uriString = "visma-identity://confirm?source=netvisor&paymentnumber=102226";
+
+        public void ParseURI()
+        {
+            var URIScheme = _uriString.Split(':');
+            Console.WriteLine(URIScheme[0]);
+            var URIAction = URIScheme[1].Split('?');
+            Console.WriteLine(URIAction[0]);
+            var UriParameters = URIAction[1].Split('=');
+            Console.WriteLine(UriParameters[1]);
+        }
+
+        public void ValidateURI()
+        {
+
+        }
+    }
+}
